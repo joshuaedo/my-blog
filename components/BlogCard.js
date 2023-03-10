3c622807e8b0297c420b5c82eae3deb8dcda792b
Joshua Edo <joshua.edo01@gmail.com>
2023-03-10 22:39:02 +0000

    Update 11 files

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/BlogCard.module.css";
+import moment from "moment";
 
 export default function BlogCard({
   title,
@@ -17,24 +18,25 @@ export default function BlogCard({
           <Image src={coverPhoto.url} alt={title} width={1000} height={1000} />
           {/* <img src={coverPhoto.url} alt={title} /> */}
         </div>
-      </Link>
-      <div className={styles.text}>
-        <h3>{title}</h3>
-        <div className={styles.details}>
-          <div className={styles.author}>
-            <Image
-              src={author.avatar.url}
-              alt={author}
-              width={55}
-              height={55}
-            />
-            <h4>{author.name}</h4>
-          </div>
-          <div className={styles.date}>
-            <p>{datePublished}</p>
+
+        <div className={styles.text}>
+          <h3>{title}</h3>
+          <div className={styles.details}>
+            <div className={styles.author}>
+              <Image
+                src={author.avatar.url}
+                alt={author}
+                width={55}
+                height={55}
+              />
+              <h4>{author.name}</h4>
+            </div>
+            <h6 className={styles.date}>
+              {moment(datePublished).format("MMMM d, YYYY")}
+            </h6>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
---
+++ pages/posts/[slug].js
@@ -0,0 +1,78 @@
+import styles from "@/styles/Slug.module.css";
+import { GraphQLClient, gql } from "graphql-request";
+
+
+const graphcms = new GraphQLClient(
+  "https://api-eu-west-2.hygraph.com/v2/clf2r6wkc3she01ug8x5v90uv/master"
+);
+
+const QUERY = gql`
+  query Post($slug: String!) {
+    post(where: { slug: $slug }) {
+      id
+      title
+      slug
+      datePublished
+      author {
+        id
+        name
+        avatar {
+          url
+        }
+      }
+      content {
+        html
+      }
+      coverPhoto {
+        id
+        url
+      }
+    }
+  }
+`;
+const SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  const { posts } = await graphcms.request(SLUGLIST);
+  return {
+    paths: posts.map((post) => ({ params: { slug: post.slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  const data = await graphcms.request(QUERY, { slug });
+  const post = data.post;
+  return {
+    props: {
+      post,
+    },
+    revalidate: 30,
+  };
+}
+
+export default function BlogPost({ post }) {
+  return (
+    <main className={styles.blog}>
+      <div className={styles.title}>
+        <div className={styles.authdetails}>
+          <div className={styles.authtext}>
+           
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: post.content.html }}
+      ></div>
+    </main>
+  );
+}
