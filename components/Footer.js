11df839e2a391e61ecc2e07ac8f95711b019628a
Joshua Edo <joshua.edo01@gmail.com>
2023-07-30 22:21:02 +0000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
     <div
       className={`flex h-10 items-center justify-between text-center font-normal pl-6 pr-9 pb-9 bebas-neue`}
     >
-      <p>{year + ` Joshua Edo • ALL RIGHTS RESERVED`}</p> <p>{wish}</p>
+      <p>&#169;{`` + year + ` Joshua Edo • ALL RIGHTS RESERVED`}</p>{" "}
+      <p>{wish}</p>
     </div>
   );
 }
